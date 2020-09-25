1992ea85154c3530ed70254c4e83727be875e828
Zaid Syed <zsyed@upatoday.com>
2020-09-25 00:15:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Main {
+  public static void main(String[] args) {
+/*
+String hello = "Hello world";
+int number = 7;
+// int uses less RAM so it's faster to run ints over doubles. 
+ System.out.println(number);
+ number++;
+  System.out.println(number);
+  number = 98;
+double decimalNumber = 3.1415;
+// double basically is a variable type with a decimal place;
+    System.out.println(number);
+*/
+
+//the only values boolean can take are true and false.
+boolean ft = true;
+false && true 
+// && statements only return true if both sides of the statement are true.
+false || true;
+// these lines mean "or".
+//using println means that the prints will come out on a new line. Using just print will mush all the outputs together. 
+
+
+  }
+}
